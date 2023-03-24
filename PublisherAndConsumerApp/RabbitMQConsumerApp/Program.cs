db7849b316939d2cca0e8b327977ac5e337b5b51
furkankekul <kekulfurkan@gmail.com>
2023-03-25 00:11:49 +0300

    RabbitMQPublisherAndConsumerApp
    Basit bir kuyruk mimarisi ile publisher uygulamasından kuyruğa iletilen mesajın consumer uygulaması ile işlenmesi

---
+++ PublisherAndConsumerApp/RabbitMQConsumerApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Channels;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Öncelikle RabbitMQ'ya bağlanıyoruz
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.HostName = "localhost";
+        //Bağlantı açıyoruz
+        IConnection connection = factory.CreateConnection();
+        //Yukarıda oluşturduğumuz bağlantı üzerinden bir kanal oluşturuyoruz.
+        IModel chanel = connection.CreateModel();
+
+        // QueueDeclare() ve içerisindeki parametreler birebir publisher uygulamasında ki verilen değerler ile aynı olmalıdır.Aksi taktirde hata alırız.
+        chanel.QueueDeclare(queue: "example", durable: true, exclusive: false, autoDelete: false);
+
+
+        /*Yukarıdaki kodlar zaten publisher kısmında yazdığımız kodlarla birebir aynı.
+         Standart bağlantı-kanal-kuyruk açma kodları. Şimdi consumer ile ilgili
+         kodlarımızı yazıyoruz.*/
+
+        //İlgili kanal üzerinden publisher uygulamasından gelen mesajları tüketecek consumer nesnesini oluşturuyoruz.
+        EventingBasicConsumer consumer = new EventingBasicConsumer(chanel);
+
+
+        //Kanal üzerinden consumer'ın hangi kuyruğu dinleyeceğini tanımlıyoruz
+        //autoAck:Kuyruktan alınan mesajların siinip silinmeme durumunu ifade eder.Eğer mesaj kuyruktan alındıktan hemen sonra kuyruktan silinirse consumer uygulamasında karşımıza çıkan bir hatadan dolayı alınan mesaj 
+        //işlenmezse burada veri kaybı meydana gelebilir o yüzden bir genel olarak autoAck'yı false olarak işaretliyelim :D
+        chanel.BasicConsume(queue: "example", autoAck: false, consumer: consumer);
+
+
+        //consumer nesnesinin Received olayı bizlere kuyruktaki mesajı getirecektir.
+        consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            Console.WriteLine("Gelen Mesaj: " + message);
+
+            /*Mesajların doğru bir şekilde işlendiğini ve kuyruktan silineceğini 
+            bildiriyoruz.multiple -> işlenmiş ama rabbitmq'dan silinmemiş 
+            mesajları da silsin istersek true demeliyiz.Biz sadece kendi 
+            mesajımızla ilgileneceğimiz için false diyoruz.*/
+            chanel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+
+        };
+        Console.Read();
+
+
+
+
+    }
+}
---
+++ PublisherAndConsumerApp/RabbitMQPublisherApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Rabbitmq sunucusuna erişmek için deaktif bağlantı nesnesini oluşturuyoruz 
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.HostName = "localhost"; 
+
+        //deaktif bağlantı nesnesini aktif hale getiriyoruz.
+
+        IConnection connection = factory.CreateConnection();
+
+        //aktif olan nesne üzerinde bir kanal oluşturuyoruz.
+        IModel chanel = connection.CreateModel();
+
+
+        //oluşturulan kanal üzerinden kuyruk oluşturuyoruz.Bu işlemi QueueDeclare metotu ile tanımlıyoruz
+        //queue -> Kuyruğumuzun adı
+        //durable(dayanıklı)->Eğer true yaparsak kuyruklarımız fiziksel,false yaparsak hafızada saklanır.
+        //exclusive(özel) ->Oluşturulacak bu kuyruğa birden fazla kanalın bağlanıp, bağlanmayacağını belirtir
+        //autoDelete:Kuyrukta yer alan veri consumer uygulamasına ulaştıktan sonra silinip silinmemesini belirtir.
+        chanel.QueueDeclare(queue: "example", durable: true, exclusive: false, autoDelete: false);
+
+
+        //Oluşturmuş olduğumuz kuyruğa mesaj yollama işlemini gerçekleştiriyoruz.
+        //oluşturacağımız mesajın türü byte olackatır kesinlikle
+        
+        string message = "examplequeue";
+        byte[] byteconvertmessage=Encoding.UTF8.GetBytes(message);
+        chanel.BasicPublish(exchange: "", routingKey: "example", body:byteconvertmessage);
+        Console.WriteLine(message+",Kuyruğa eklendi");
+
+        Console.Read();
+
+
+        //Yukarıdaki işlemler neticesinde oluşturmuş olduğumuz example kuyruğuna message gönderme işlemini tamamlamış oluyoruz .
+
+
+
+    }
+}
